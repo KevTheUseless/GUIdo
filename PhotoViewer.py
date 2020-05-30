37e258df5045de899e2bd8e45f33ec3505d54deb
pythonleo <shizhan8226@163.com>
2020-05-30 15:54:11 +0800

    add button active status

--- PhotoViewer.py
+++ PhotoViewer.py
@@ -56,8 +56,8 @@ class Guide(object):
 		secret = Secret(rect, guideID)
 		self.secretList.append(secret)
 	def mouseDown(self, pos, button):
-		for button in self.btnList:
-			button.mouseDown(pos, button)
+		for btn in self.btnList:
+			btn.mouseDown(pos, button)
 		for secret in self.secretList:
 			secret.mouseDown(pos, button)
 	def mouseUp(self, pos, button):
@@ -65,25 +65,26 @@ class Guide(object):
 			button.mouseUp(pos, button)
 	def mouseMotion(self, pos):
 		framework.mousePos = pos
-		return
+		for btn in self.btnList:
+			btn.mouse_move(pos)
 
 class Button(object):
 	def __init__(self, name, picFile, x, y, guideID):
 		self.name = name
 		self.img = pygame.image.load(picFile)
-		self.img.set_colorkey((38, 38, 38))
-		self.w, self.h = self.img.get_width() // 2, self.img.get_height()
+		self.img.set_colorkey((0, 255, 0))
+		self.w, self.h = self.img.get_width() // 3, self.img.get_height()
 		self.x, self.y = x, y
 		self.rect = pygame.Rect(self.x, self.y, self.w, self.h)
 		self.status = 0
 		self.guideID = guideID
 	def draw(self, screen):
-		screen.blit(self.img, (self.x, self.y), \
-					(self.status * self.rect.w, 0, \
+		screen.blit(self.img, (self.x, self.y),
+					(self.status * self.rect.w, 0,
 					 self.rect.w, self.rect.h))
 	def mouseDown(self, pos, button):
 		if self.rect.collidepoint(pos):
-			self.status = 1
+			self.status = 2
 	def mouseUp(self, pos, button):
 		self.status = 0
 		if not self.rect.collidepoint(pos):
@@ -97,6 +98,11 @@ class Button(object):
 		if self.name == 'R':
 			framework.guideList[self.guideID].pic.draw(framework.screen, 4, framework.speed)
 		framework.guideID = self.guideID
+
+	def mouse_move(self, pos):
+		if self.rect.collidepoint(pos):
+			self.status = 1
+		else: self.status = 0
 
 class Txt(object):
 	def __init__(self, txt, font, x, y, c, rect):
@@ -128,12 +134,12 @@ g03 = Guide("res/xian03.jpg")
 framework.addGuide(g03)
 g04 = Guide("res/xian04.jpg")
 framework.addGuide(g04)
-g01.addButton('U', "res/button/up.bmp", width // 2 - 35, 20, g02.id)
-g01.addButton('L', "res/button/left.bmp", 20, height // 2 - 35, g03.id)
-g01.addButton('R', "res/button/right.bmp", width - 100, height // 2 - 35, g04.id)
-g02.addButton('D', "res/button/down.bmp", width // 2 - 35, height - 100, g01.id)
-g03.addButton('R', "res/button/right.bmp", width - 100, height // 2 - 35, g01.id)
-g04.addButton('L', "res/button/left.bmp", 20, height // 2 - 35, g01.id)
+g01.addButton('U', "res/button/btn_up.bmp", width // 2 - 35, 20, g02.id)
+g01.addButton('L', "res/button/btn_left.bmp", 20, height // 2 - 35, g03.id)
+g01.addButton('R', "res/button/btn_right.bmp", width - 100, height // 2 - 35, g04.id)
+g02.addButton('D', "res/button/btn_down.bmp", width // 2 - 35, height - 100, g01.id)
+g03.addButton('R', "res/button/btn_right.bmp", width - 100, height // 2 - 35, g01.id)
+g04.addButton('L', "res/button/btn_left.bmp", 20, height // 2 - 35, g01.id)
 g01.addTxt("How are you?", framework.font32, 487, 407, (0, 255, 0), (580, 439, 26, 38))
 
 g05 = Guide("res/profilePic.jpg")
