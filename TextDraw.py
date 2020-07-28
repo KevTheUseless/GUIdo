a8f5a0b450b35c58df41b3aa5f4ecd32a2ecff82
The-UltimateGamer <2357931342@qq.com>
2020-07-28 21:23:22 +0800

    Text input system complete

--- TextDraw.py
+++ TextDraw.py
@@ -6,11 +6,63 @@ import pygame, sys
 width, height = 800, 600
 
 txtBuffer = []
+caps = {
+	'`': '~',
+	'1': '!',
+	'2': '@',
+	'3': '#',
+	'4': '$',
+	'5': '%',
+	'6': '^',
+	'7': '&',
+	'8': '*',
+	'9': '(',
+	'0': ')',
+	'-': '_',
+	'=': '+',
+	'[': '{',
+	']': '}',
+	'\\': '|',
+	';': ':',
+	'\'': '"',
+	',': '<',
+	'.': '>',
+	'/': '?',
+	'a': 'A',
+	'b': 'B',
+	'c': 'C',
+	'd': 'D',
+	'e': 'E',
+	'f': 'F',
+	'g': 'G',
+	'h': 'H',
+	'i': 'I',
+	'j': 'J',
+	'k': 'K',
+	'l': 'L',
+	'm': 'M',
+	'n': 'N',
+	'o': 'O',
+	'p': 'P',
+	'q': 'Q',
+	'r': 'R',
+	's': 'S',
+	't': 'T',
+	'u': 'U',
+	'v': 'V',
+	'w': 'W',
+	'x': 'X',
+	'y': 'Y',
+	'z': 'Z'
+}
 
 pygame.init()
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("TextDraw")
 clock = pygame.time.Clock()
+
+shift = False
+capsLock = False
 
 while True:
 	pygame.display.update()
@@ -30,14 +82,21 @@ while True:
 			elif event.key == pygame.K_RETURN:
 				txtBuffer.append('\n')
 				print(txtBuffer)
-			elif event.key == pygame.K_LSHIFT or event == pygame.K_RSHIFT:
-				# TODO: do shift
-				pass
+			elif event.key == pygame.K_LSHIFT or event.key == pygame.K_RSHIFT:
+				shift = True
 			elif event.key == pygame.K_CAPSLOCK:
-				# TODO: always do shift
-				pass
+				capsLock = 1 - capsLock
 			else:
 				if 32 <= event.key <= 126 or event.key == pygame.K_TAB:
-					txtBuffer.append(chr(event.key))
-					for c in txtBuffer:
-						print(txtBuffer)
+					if (44 <= event.key <= 57 or event.key == 59 or event.key == 61 or event.key == 96) and shift:
+						txtBuffer.append(caps[chr(event.key)])
+					elif 97 <= event.key <= 122 and (shift or capsLock):
+						txtBuffer.append(caps[chr(event.key)])
+					else:
+						txtBuffer.append(chr(event.key))
+		elif event.type == pygame.KEYUP:
+			if event.key == pygame.K_LSHIFT or event.key == pygame.K_RSHIFT:
+				shift = False
+			elif event.key == pygame.K_CAPSLOCK:
+				capsLock = 1 - capsLock
+	print(txtBuffer, shift, capsLock)
